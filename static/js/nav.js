a7b3268e25374aa744339eb52e7ce7628e95e955
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-10-24 11:41:50 +0000

    Add: credits to nav.css and nav.js

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,3 +1,6 @@
+/* Adapted from Online Tutorials YouTube channel - 
+'Magic Navigation Menu Indicator using HTML, CSS & Javascript | Curve Outside Effects */
+
 const list = document.querySelectorAll('.list');
 function activeLink() {
     list.forEach((item) => item.classList.remove('active'));
