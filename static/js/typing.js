93b1efe6439724110e8f684b37263fc4a2db2c73
NickChapman1988 <muppetnj_uk@hotmail.com>
2023-11-10 16:48:46 +0000

    Add: jQuery and typed.js scripts, typing animation script

---
+++ static/js/typing.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings: ["","Web Developer", "Web Designer", "Problem Solver", "Freelancer", "Gamer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
